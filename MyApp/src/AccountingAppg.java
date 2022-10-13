3dd2ca7f6668158111df6f2325f432227d722675
kim sangwon <114643228+Ssangwon2@users.noreply.github.com>
2022-10-13 20:38:00 +0900

    Add files via upload

---
+++ MyApp/src/AccountingAppg.java
@@ -0,0 +1,30 @@
+
+public class AccountingAppg {
+
+	public static void main(String[] args) {
+	
+	
+	double valueOfSupply = Double.parseDouble(args[0]);
+	double VatRate = 0.1;
+	double expenseRate = 0.3;
+	double vat = valueOfSupply * VatRate;
+	double totall = valueOfSupply + vat;
+	double expense = valueOfSupply * expenseRate;
+	double income = valueOfSupply - expense;
+	double dividend1 =income*0.5;
+	double dividend2 = income*0.3;
+	double dividend3 = income*0.2;
+  	
+	System.out.println("Value of supply : " + valueOfSupply);
+	System.out.println("VAT : " + vat);
+	System.out.println("Totall : " + totall);
+	System.out.println("Expense : " + expense );
+	System.out.println("Income : " + income);
+	System.out.println("Dividend : " + dividend1  );
+	System.out.println("Dividend : " + dividend2  );
+	System.out.println("Dividend : " + dividend3  );
+
+
+	}
+
+}
